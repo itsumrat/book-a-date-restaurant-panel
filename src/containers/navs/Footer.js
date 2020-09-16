c11f320781038d412bc165adee9fe6e9eb1f63da
masum millat <masum.millat@cramstack.com>
2020-09-17 00:21:56 +0600

    changes

--- src/containers/navs/Footer.js
+++ src/containers/navs/Footer.js
@@ -12,25 +12,25 @@ const Footer = () => {
             <Colxx xxs="12" sm="6">
               <p className="mb-0 text-muted">ColoredStrategies 2020</p>
             </Colxx>
-            <Colxx className="col-sm-6 d-none d-sm-block">
-              <ul className="breadcrumb pt-0 pr-0 float-right">
-                <li className="breadcrumb-item mb-0">
-                  <NavLink className="btn-link" to="#" location={{}}>
-                    Review
-                  </NavLink>
-                </li>
-                <li className="breadcrumb-item mb-0">
-                  <NavLink className="btn-link" to="#" location={{}}>
-                    Purchase
-                  </NavLink>
-                </li>
-                <li className="breadcrumb-item mb-0">
-                  <NavLink className="btn-link" to="#" location={{}}>
-                    Docs
-                  </NavLink>
-                </li>
-              </ul>
-            </Colxx>
+            {/*<Colxx className="col-sm-6 d-none d-sm-block">*/}
+            {/*  <ul className="breadcrumb pt-0 pr-0 float-right">*/}
+            {/*    <li className="breadcrumb-item mb-0">*/}
+            {/*      <NavLink className="btn-link" to="#" location={{}}>*/}
+            {/*        Review*/}
+            {/*      </NavLink>*/}
+            {/*    </li>*/}
+            {/*    <li className="breadcrumb-item mb-0">*/}
+            {/*      <NavLink className="btn-link" to="#" location={{}}>*/}
+            {/*        Purchase*/}
+            {/*      </NavLink>*/}
+            {/*    </li>*/}
+            {/*    <li className="breadcrumb-item mb-0">*/}
+            {/*      <NavLink className="btn-link" to="#" location={{}}>*/}
+            {/*        Docs*/}
+            {/*      </NavLink>*/}
+            {/*    </li>*/}
+            {/*  </ul>*/}
+            {/*</Colxx>*/}
           </Row>
         </div>
       </div>
